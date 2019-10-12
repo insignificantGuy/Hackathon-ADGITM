491da0f7b70a8cbbe7be5923f50675a8ccadbc16
Insignificant_Guy <zakramisaway@gmail.com>
2019-10-13 00:51:18 +0530

    inital commit

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var app = express();
-app.engine('handlebars',exphbs());
+app.engine('handlebars', exphbs());
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'vendor')));
 
 //body-parser
 app.use(bodyParser.urlencoded({
-  extended:false
+  extended: false
 }));
 app.use(bodyParser.json());
 console.log("Post Service Acquired")
@@ -34,86 +34,91 @@ const ObjectId = Schema.ObjectId;
 
 const userschema = new Schema({
 
-  first:String,
-  last:String,
-  email:String,
-  phone:String,
-  type:String,
-  pass:String,
+  first: String,
+  last: String,
+  email: String,
+  phone: String,
+  type: String,
+  pass: String,
 });
 
 const usermodel = mongoose.model("user", userschema);
 
-app.get('/', function(req, res){
-	res.render("index");
+app.get('/', function (req, res) {
+  res.render("index");
 });
 
-app.get('/contact',(req,res) =>{
+app.get('/contact', (req, res) => {
   res.render("contact");
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
   res.render("about");
 });
 
-app.get('/causes',(req,res)=>{
+app.get('/causes', (req, res) => {
   res.render("causes");
 });
 
-app.get('/services',(req,res)=>{
-  res.render("services");
+app.get('/facts', (req, res) => {
+  res.render("facts");
 });
 
-app.get('/login',(req,res)=>{
-  res.render('login',{mesg:null})
+app.get('/login', (req, res) => {
+  res.render('login', { mesg: null })
 })
 
-app.post('/login',(req,res)=>{
-  usermodel.findOne({ email: req.body.email, pass: req.body.pass }, function (err, doc) {
-    if (err) {
+app.post('/login', (req, res) => {
+  if (req.body.submit) {
+    let a = req.body.email
+    let b = req.body.pass
+    let c = req.body.subject
+    usermodel.findOne({ email: a, pass: b, }, function (err, doc) {
+      console.log(doc)
+      if (err) {
         console.log(err, 'error')
-        res.redirect('/login')
+        res.render('login', { mesg: "An Error Occurred, Please try again after some time." })
         return
-    }
-    if (_.isEmpty(doc)) {
+      }
+      if (_.isEmpty(doc)) {
         res.render('login', { mesg: "Please check email/password" })
-    } else {
-        res.render('scanner',{mesg:doc.name,amount:"1500"})
-    }
-})
+      } else {
+        res.redirect('/scanner')
+      }
+    })
+  }
 })
 
-app.get('/register',(req,res)=>{
+app.get('/register', (req, res) => {
   res.render('signup')
 })
 
-app.post('/register',(req,res)=>{
+app.post('/register', (req, res) => {
   console.log(req.body);
-  if(req.body.submit){
+  if (req.body.submit) {
     let newuser = new usermodel()
-        newuser.first = req.body.first_name
-        newuser.last = req.body.last_name
-        newuser.email = req.body.email
-        newuser.phone = req.body.phone
-        newuser.type = req.body.subject
-        newuser.pass=req.body.pass
-        newuser.save(function (err) {
-            if (err) {
-                console.log(err)
-                return
-            }
-            else {
-                res.render('login',{mesg:"You have Been Registered"})
-            }
-        });
+    newuser.first = req.body.first_name
+    newuser.last = req.body.last_name
+    newuser.email = req.body.email
+    newuser.phone = req.body.phone
+    newuser.type = req.body.subject
+    newuser.pass = req.body.pass
+    newuser.save(function (err) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      else {
+        res.redirect('/login')
+      }
+    });
   }
-  res.render('scanner',{mesg:null,amount:"0"})
 })
 
-app.get('/scanner',(req,res)=>{
-  res.render('scanner',{message:"null",amount:"null"})
+app.get('/scanner/:doc.email', (req, res) => {
+  res.render('scanner', { message: "null", amount: "null" })
 })
 
-app.listen(PORT,(req,res) => {
-	console.log('App on 3000');
+app.listen(PORT, (req, res) => {
+  console.log('App on 3000');
 });
